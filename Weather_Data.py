5755774a1d962d59a5e817d1dd1b6cc3c7348326
Joven-Kvortek <kvortekj@oregonstate.edu>
2025-02-14 10:53:46 -0800

    Full Microservice

--- Weather_Data.py
+++ Weather_Data.py
@@ -1,32 +1,67 @@
 import json
 import random
-
-random_temp = random.randint(0,100)
-
-random_humidity = random.randint(0,100)
-
-conditions = ["Clear skies", "Cloudy", "Thunderstorm", "Rain", "Snow"]
-random_condition = random.choice(conditions)
-
-random_precipitation = 0
-if random_condition == "Thunderstorm" or random_condition == "Rain" or random_condition == "Snow":
-    random_precipitation = random.randint(50,100)
-else:
-    random_precipitation = random.randint(0, 15)
-
-random_windspeed = random.randint(0,50)
-wind_direction = ["North", "South", "East", "West", "Northeast", "Southeast", "Northwest", "Southwest"]
+import datetime
+import pika
 
 
 
 
-weather_data = {
-    "Temperature": f"{random_temp} degrees",
-    "Condition": f"{random_condition}",
-    "Humidity": f"{random_humidity}%",
-    "Precipitation": f"{random_precipitation}%",
-    "WindSpeed": f"{random_windspeed}mph",
-    "WindDirection": f"{random.choice(wind_direction)}"
-}
-with open("Data.json", "w") as outfile:
-    json.dump(weather_data, outfile)
+def random_weather():
+    random_temp = random.randint(0,100)
+
+    random_humidity = random.randint(0,100)
+
+    conditions = ["Clear skies", "Cloudy", "Thunderstorm", "Rain", "Snow"]
+    random_condition = random.choice(conditions)
+
+    random_precipitation = 0
+    if random_condition == "Thunderstorm" or random_condition == "Rain" or random_condition == "Snow":
+        random_precipitation = random.randint(50,100)
+    else:
+        random_precipitation = random.randint(0, 15)
+
+    random_windspeed = random.randint(0,50)
+    wind_direction = ["North", "South", "East", "West", "Northeast", "Southeast", "Northwest", "Southwest"]
+
+    todays_date = datetime.datetime.now()
+    todays_date_ISO = todays_date.isoformat("#", "seconds")
+    print(todays_date_ISO)
+
+
+    weather_data = {
+        "Time and Date": f"{todays_date_ISO}",
+        "Temperature": f"{random_temp} degrees",
+        "Condition": f"{random_condition}",
+        "Humidity": f"{random_humidity}%",
+        "Precipitation": f"{random_precipitation}%",
+        "WindSpeed": f"{random_windspeed}mph",
+        "WindDirection": f"{random.choice(wind_direction)}"
+    }
+    return weather_data
+
+def on_request(ch, method, props, body):
+    print("Weather request received")
+    weather_data = random_weather()
+    response_body = json.dumps(weather_data)
+    print(response_body)
+    print("Weather request received, sending response: ", response_body)
+
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id = props.correlation_id),
+                     body=response_body
+
+                     )
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+def start_server():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='weather')
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue='weather', on_message_callback=on_request)
+    print("Waiting for messages. To exit press CTRL+C")
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    start_server()
---
+++ receive.py
@@ -0,0 +1,44 @@
+import uuid
+from ast import main
+import json
+import pika, sys, os
+
+class WeatherRPC:
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.channel = self.connection.channel()
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(queue=self.callback_queue,
+                                   on_message_callback=self.on_response,
+                                   auto_ack=True
+                                    )
+        self.response = None
+        self.corr_id = None
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def request(self):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(exchange='',
+                                   routing_key='weather',
+                                   properties=pika.BasicProperties(
+                                         reply_to = self.callback_queue,
+                                         correlation_id = self.corr_id,
+                                         ),
+                                   body = ''
+                                   )
+        while self.response is None:
+            self.connection.process_data_events()
+
+        return json.loads(self.response.decode())
+
+if __name__ == '__main__':
+    weather_client = WeatherRPC()
+    weather_data = weather_client.request()
+
+
